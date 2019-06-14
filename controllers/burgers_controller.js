17e9fc54662af7f793e0072fc79525be2225c260
Amanda Dovel <amandacdovel@gmail.com>
2019-06-13 21:48:18 -0600

    update with files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+router.get()
+
+router.post()
+
+router.put()
+
+router.delete()
+
+// Export routes for server.js to use.
+module.exports = router;
